a64d5a93e35c93f2bde830bdc12e0497e6d94e40
SergRudovich <slunrew@tut.by>
2021-07-28 15:00:27 +0300

    Frontend: fix move router to AppProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
@@ -8,9 +7,7 @@ import './services/i18n';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
