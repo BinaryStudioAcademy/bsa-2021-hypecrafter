45d6581ee61d32cf4ab4647952739fa39df700d1
Cuba49 <kubovych.kostya59@gmail.com>
2021-07-28 10:38:11 +0300

    Update i18n.ts

--- frontend/src/services/i18n.ts
+++ frontend/src/services/i18n.ts
@@ -2,13 +2,14 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import dictionaries from '../dictionaries';
+import { Languages } from '../common/enums';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     debug: true,
-    fallbackLng: 'en',
+    fallbackLng: Languages.UA,
     interpolation: {
       escapeValue: false
     },
