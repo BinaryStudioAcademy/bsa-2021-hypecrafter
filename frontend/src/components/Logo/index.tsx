2d4ce97691b748e6e60bef840cd705e21be078e0
dgorkavyi <dima.net.dd@gmail.com>
2021-07-28 21:52:46 +0300

    glitched logo created

---
+++ frontend/src/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import './logo.css';
+
+const Logo = () => (
+  <div className="logo glitch">
+    <span aria-hidden="true">HypeCrafter</span>
+    <span aria-hidden="true">HypeCrafter</span>
+    <span aria-hidden="true">HypeCrafter</span>
+    <span aria-hidden="true">HypeCrafter</span>
+  </div>
+);
+
+export default Logo;
