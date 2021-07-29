72e47669167e8e70cf3c6c498f4703bb2da66453
Cuba49 <kubovych.kostya59@gmail.com>
2021-07-29 13:51:19 +0300

    add fonts and set up base styles

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
 import App from './components/App';
 import './services/i18n';
 
