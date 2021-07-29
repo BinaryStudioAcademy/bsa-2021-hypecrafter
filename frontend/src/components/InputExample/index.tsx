356767f81680ec0bc8ac859adaab01ba403ecb80
Nikita <nik2142ua@gmail.com>
2021-07-29 15:17:03 +0300

    Add InputExample component

---
+++ frontend/src/components/InputExample/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { Container, Col } from 'react-bootstrap';
+import Input from '../Input';
+
+function InputExample() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [text, setText] = useState('');
+
+  return (
+    <Container>
+      <Col md="4">
+        <Input type="text" value={text} placeholder="Search..." onChange={setText} />
+        <Input
+          type="email"
+          value={email}
+          placeholder="Enter your email"
+          label="Email"
+          onChange={setEmail}
+          errorMessage="Error example."
+        />
+        <Input
+          type="password"
+          value={password}
+          placeholder="Enter your password"
+          label="Password"
+          onChange={setPassword}
+        />
+      </Col>
+    </Container>
+  );
+}
+
+export default InputExample;
--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -1,44 +1,20 @@
-import { useState } from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Users from '../../scenes/Users';
 import BootstrapExample from '../BootstrapExample';
-import Input from '../Input';
 import TranslateExample from '../TranslateExample';
 import ButtonExample from '../ButtonExample';
+import InputExample from '../InputExample';
 
-const Main = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [text, setText] = useState('');
-
+function Main() {
   return (
     <Container>
       <Users />
       <TranslateExample />
       <BootstrapExample />
       <ButtonExample />
-      <Container>
-        <Col md="4">
-          <Input type="text" value={text} placeholder="Search..." onChange={setText} />
-          <Input
-            type="email"
-            value={email}
-            placeholder="Enter your email"
-            label="Email"
-            onChange={setEmail}
-            errorMessage="Error example."
-          />
-          <Input
-            type="password"
-            value={password}
-            placeholder="Enter your password"
-            label="Password"
-            onChange={setPassword}
-          />
-        </Col>
-      </Container>
+      <InputExample />
     </Container>
   );
-};
+}
 
 export default Main;
