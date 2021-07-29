764a73022e1356e0914c8b642927971455d10248
eluyemaStudy <elluyema@gmail.com>
2021-07-29 14:48:27 +0300

    Add documentation on example user routers and removed unneeded modules

--- gateway/src/server.ts
+++ gateway/src/server.ts
@@ -7,36 +7,17 @@ import { handleError, logger } from './api/middlewares';
 import initRoutes from './api/routes';
 import { env } from './env';
 import swaggerUI from 'swagger-ui-express'
-import swaggerJsdoc from 'swagger-jsdoc'
+import openApiDocumentation from '../openApiDocumentation.json';
 const { port, environment, rabbit } = env.app;
-
 const gateway = new Gateway({
   microservices: [Project.BACKEND, Project.PAYMENT],
   rabbit
 });
-console.log("---------",port)
-const swaggerOptions = {
-  definition:{
-    openapi: '3.0.0',
-    info:{
-      title: 'Library API',
-      version: '1.0.0'
-    },
-    servers:[
-      {
-        url: 'http://localhost:'+port
-      }
-    ]
-  },
-  apis:["../../backend/src/api/routes/user.ts"]
-}
-const specs = swaggerJsdoc(swaggerOptions)
 
 
-console.log(environment)
 const app = express();
 
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(specs));
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(openApiDocumentation));
 app.use(cors());
 app.use(logger);
 app.use(json());
