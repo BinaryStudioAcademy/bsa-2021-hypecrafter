db940bd379350a9fd7eff59789cddc36e0af09e0
Liza <liza140620002@gmail.com>
2021-07-29 16:32:36 +0300

    aremove asyncForEach

--- gateway/src/helpers/arrayHelper.ts
+++
@@ -1,6 +0,0 @@
-export const asyncForEach = async <T>(fn: (x: T) => Promise<void>, list: readonly T[]) => {
-  for (let i = 0; i < list.length; i += 1) {
-    // eslint-disable-next-line no-await-in-loop
-    await fn(list[i]);
-  }
-};
