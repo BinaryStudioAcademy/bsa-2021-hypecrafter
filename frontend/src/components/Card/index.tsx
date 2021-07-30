c527db34cf7112994b2da9173310e4193da649b8
Ivan Tur <ivantur2@gmail.com>
2021-07-30 23:07:01 +0300

    update Card component

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,10 +1,10 @@
 import { FC } from 'react';
-import { ProgressBar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import cn from 'classnames';
 import classes from './styles.module.scss';
+import Tag from '../Tag';
+import ProgressBar from '../ProgressBar';
 
-interface Props {
-  to: string;
+type Props = {
   category: string;
   tags: string[];
   name: string;
@@ -12,47 +12,36 @@ interface Props {
   goal: number;
   percent: number;
   image: string;
+  rounded?: boolean;
 }
 
 const CardComponent: FC<Props> = ({
-  to,
   category,
   tags,
   name,
   description,
   goal,
   percent,
-  image
+  image,
+  rounded = false
 }) => (
-  <Link className={classes.link} to={to}>
-    <div className={classes.card}>
-      <div className={classes.cardImage}>
-        <img src={image} alt="card img" />
-      </div>
-
-      <div className={classes.cardContent}>
-        <div className={classes.category}>{category}</div>
-        <div className={classes.title}>{name}</div>
-        <div className={classes.description}>{description}</div>
-
-        <div className={classes.tags}>
-          {tags.map((tag => (
-            <div className={classes.tag}>
-              {tag}
-            </div>
-          )))}
-        </div>
-
-        <div className={classes.progress}>
-          <div className={classes.progressInfo}>
-            <div className={classes.goal}>{`$ ${goal}`}</div>
-            <div className={classes.percent}>{`${percent}%`}</div>
-          </div>
-          <ProgressBar className={classes.progressBar} now={percent} label={`${percent}%`} visuallyHidden />
-        </div>
-      </div>
+  <article className={cn(classes.card, { [classes.rounded]: rounded })}>
+    <div className={classes.cardImage}>
+      <img src={image} alt="card img" />
     </div>
-  </Link>
+
+    <div className={classes.cardContent}>
+      <div className={classes.category}>{category}</div>
+      <div className={classes.title}>{name}</div>
+      <div className={classes.description}>{description}</div>
+
+      <div className={classes.tags}>
+        {tags.map((tag => <Tag text={tag} />))}
+      </div>
+
+      <ProgressBar goal={goal} percent={percent} />
+    </div>
+  </article>
 );
 
 export default CardComponent;
