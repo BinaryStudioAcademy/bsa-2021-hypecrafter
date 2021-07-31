a694d8d7bc03feafa3b92a620cfe58e418d655e5
eluyemaStudy <elluyema@gmail.com>
2021-08-01 01:20:00 +0300

    add styles and change footer to optional

--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Users from '../../scenes/Users';
 import BootstrapExample from '../BootstrapExample';
 import TranslateExample from '../TranslateExample';
 import ButtonExample from '../ButtonExample';
 import InputExample from '../InputExample';
+import ModalWindow from '../ModalWindow';
 
 function Main() {
+  const [show, setShow] = useState(true);
   return (
     <Container>
       <Users />
+      <ModalWindow
+        show={show}
+        title="Modal"
+        body="Hello there!"
+        showTrigger={() => {
+          setShow(false);
+        }}
+      />
       <TranslateExample />
       <BootstrapExample />
       <ButtonExample />
--- frontend/src/components/ModalWindow/index.tsx
+++ frontend/src/components/ModalWindow/index.tsx
@@ -1,23 +1,23 @@
-/*  eslint linebreak-style: ["error", "windows"]    */
 import React, { ReactNode } from 'react';
 import { Modal } from 'react-bootstrap';
+import './modal.scss';
 
 interface ModalWindowProps {
   show: boolean;
   title: string | ReactNode;
   body: string | ReactNode;
-  footer: ReactNode | null;
+  footer?: string | ReactNode;
   showTrigger(): void;
 }
 const ModalWindow: React.FC<ModalWindowProps> = (props) => {
   const { title, show, body, footer, showTrigger } = props;
   return (
-    <Modal show={show} onHide={showTrigger}>
+    <Modal show={show} onHide={showTrigger} className="my-modal">
       <Modal.Header closeButton>
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>{body}</Modal.Body>
-      <Modal.Footer>{footer}</Modal.Footer>
+      {footer && <Modal.Footer>{footer}</Modal.Footer>}
     </Modal>
   );
 };
