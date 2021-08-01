19a64dcf2b7bddef843fd8e133cfee0e8fc62e91
Ivan Tur <ivantur2@gmail.com>
2021-08-01 05:05:47 +0300

    add login page

---
+++ frontend/src/components/LoginPage/index.tsx
@@ -0,0 +1,75 @@
+import { FC, FormEventHandler, MouseEventHandler, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../Button';
+import Input from '../Input';
+import classes from './styles.module.scss';
+import logo from '../../assets/HypeCrafter.svg';
+import { Routes } from '../../common/enums';
+
+const LoginPage: FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const dummySignInHandler: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log('Sign In');
+  };
+
+  const dummySignInWithGoogleHandler: MouseEventHandler<HTMLButtonElement> = (e) => {
+    console.log('Sign In with Google');
+  };
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.hero}>
+        <img className={classes.logo} src={logo} alt="logo" />
+      </div>
+
+      <div className={classes.content}>
+        <form className={classes.form} onSubmit={dummySignInHandler}>
+          <h2 className={classes.title}>Sign In</h2>
+          <div className={classes['register-cta']}>
+            Don’t have an account? <Link to={Routes.SIGNUP}>Sign Up</Link>
+          </div>
+
+          <Input
+            type="email"
+            placeholder="Enter your email"
+            label="Email"
+            value={email}
+            onChange={setEmail}
+            errorMessage=""
+          />
+
+          <div className={classes['password-cta']}>
+            <Link to={Routes.RESET_PASSWORD}>Forgot your password?</Link>
+          </div>
+          <Input
+            type="password"
+            placeholder="......."
+            value={password}
+            label="Password"
+            onChange={setPassword}
+            errorMessage=""
+          />
+
+          <Button
+            className={classes['sign-in-button']}
+            type="submit"
+          >
+            Sign In
+          </Button>
+          <hr className={classes['horizontal-ruler']} />
+          <Button
+            className={classes['google-button']}
+            onClick={dummySignInWithGoogleHandler}
+          >
+            Sign In with Google
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- frontend/src/components/Routing/index.tsx
+++ frontend/src/components/Routing/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Switch } from 'react-router-dom';
 import { Routes } from '../../common/enums';
+import LoginPage from '../LoginPage';
 import Main from '../Main';
 
 const Routing = () => (
   <Switch>
     <Route path={Routes.HOME} exact component={Main} />
+    <Route path={Routes.LOGIN} exact component={LoginPage} />
   </Switch>
 );
 
