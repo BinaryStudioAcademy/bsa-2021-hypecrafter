14a197dca2189d2329d3781960f6da57c1f409b5
Nikita <nik2142ua@gmail.com>
2021-08-02 17:44:35 +0300

    Add user-page option to modal

--- frontend/src/components/ModalWindow/index.tsx
+++ frontend/src/components/ModalWindow/index.tsx
@@ -7,7 +7,7 @@ interface ModalWindowProps {
   title: string | ReactNode;
   body: string | ReactNode;
   footer?: string | ReactNode;
-  size: 'small' | 'medium' | 'wide';
+  size: 'small' | 'medium' | 'wide' | 'user-page';
   centered?: boolean;
   onHide(): void;
 }
