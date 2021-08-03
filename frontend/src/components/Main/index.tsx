c334229f273a42b2877b29cae922ff8b31b5feaf
dgorkavyi <dima.net.dd@gmail.com>
2021-08-03 13:51:34 +0300

    resolved conflicts

--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Users from '../../scenes/Users';
 import BootstrapExample from '../BootstrapExample';
 import TranslateExample from '../TranslateExample';
 import ButtonExample from '../ButtonExample';
 import InputExample from '../InputExample';
+import ModalWindow from '../ModalWindow';
 import Chart from '../Chart/Chart';
 import defaultProps from '../Chart/testprops';
 
 function Main() {
+  const [show, setShow] = useState(true);
   return (
     <Container>
       <Chart
@@ -16,6 +19,14 @@ function Main() {
         dataSets={defaultProps.data.datasets}
       />
       <Users />
+      <ModalWindow
+        show={show}
+        title="Modal"
+        body="Hello there!"
+        onHide={() => {
+          setShow(false);
+        }}
+      />
       <TranslateExample />
       <BootstrapExample />
       <ButtonExample />
