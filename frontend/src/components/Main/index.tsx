eef8c0b3f72325a9b7134067f4255afbd22afda3
Demetreus <dima.net.dd@gmail.com>
2021-08-03 15:41:12 +0300

    add Chart component to Main

--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -8,11 +8,18 @@ import InputExample from '../InputExample';
 import PopoverExample from '../PopoverExample';
 import CardExample from '../CardExample';
 import ModalWindow from '../ModalWindow';
+import Chart from '../Chart/Chart';
+import defaultProps from '../Chart/testprops';
 
 function Main() {
   const [show, setShow] = useState(true);
   return (
     <Container>
+      <Chart
+        type={defaultProps.type}
+        labels={defaultProps.data.labels}
+        dataSets={defaultProps.data.datasets}
+      />
       <Users />
       <PopoverExample />
       <ModalWindow
