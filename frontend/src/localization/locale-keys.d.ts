900e7f071a2f0d84f2d142e26ce02145984be3a7
Liza <liza140620002@gmail.com>
2021-08-04 18:46:58 +0300

    change name of script in package.json

--- frontend/src/localization/locale-keys.d.ts
+++ frontend/src/localization/locale-keys.d.ts
@@ -1,12 +1,12 @@
 declare type LocaleKeys =
-  | 'Press on me'
-  | 'Sign In'
-  | 'Sign Up'
-  | 'Email'
-  | 'Password'
-  | 'Don’t have an account?'
-  | 'Enter your email'
-  | 'Enter your password'
-  | 'Forgot your password?'
-  | 'or'
-  | 'Sign In with Google';
+ | 'Press on me'
+ | 'Sign In'
+ | 'Sign Up'
+ | 'Email'
+ | 'Password'
+ | 'Don’t have an account?'
+ | 'Enter your email'
+ | 'Enter your password'
+ | 'Forgot your password?'
+ | 'or'
+ | 'Sign In with Google';
