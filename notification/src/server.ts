de3aac1368906d707f26000ca040e0df0e3e4acc
dgorkavyi <dima.net.dd@gmail.com>
2021-08-06 13:21:37 +0300

    added job schedule lib and an example in server.ts

--- notification/src/server.ts
+++ notification/src/server.ts
@@ -1,6 +1,7 @@
 import { createConnection } from 'typeorm';
 import MicroMq from 'micromq';
 import { Project } from 'hypecrafter-shared/enums';
+import * as cron from 'node-cron';
 import { log } from './helpers/logger';
 import initRoutes from './api/routes';
 import { env } from './env';
@@ -11,6 +12,18 @@ const app = new MicroMq({
   name: Project.NOTIFICATION,
   rabbit
 });
+
+cron.schedule(
+  '*/1 * * * *',
+  () => {
+    const hh = new Date().getHours();
+    const mm = new Date().getMinutes();
+    console.log(
+      `this message must shows every minute(current time is ${hh}:${mm})`
+    );
+  },
+  { scheduled: true }
+);
 
 createConnection()
   .then(() => {
