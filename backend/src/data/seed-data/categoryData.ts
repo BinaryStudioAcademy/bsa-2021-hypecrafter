2c7b8b010dd72b5f21fd3a7e2f56b4e58ad63ad1
maksim-luzin <333maximax333@gmail.com>
2021-08-06 13:18:13 +0300

    Add category seed

---
+++ backend/src/data/seed-data/categoryData.ts
@@ -0,0 +1,52 @@
+import { CategoryType } from '../../common/enums';
+
+export const categories = [
+  {
+    name: CategoryType.Art
+  },
+  {
+    name: CategoryType.Comics
+  },
+  {
+    name: CategoryType.Crafts
+  },
+  {
+    name: CategoryType.Dance
+  },
+  {
+    name: CategoryType.Design
+  },
+  {
+    name: CategoryType.Fashion
+  },
+  {
+    name: CategoryType.FilmAndVideo
+  },
+  {
+    name: CategoryType.Food
+  },
+  {
+    name: CategoryType.Games
+  },
+  {
+    name: CategoryType.Journalism
+  },
+  {
+    name: CategoryType.Music
+  },
+  {
+    name: CategoryType.Photography
+  },
+  {
+    name: CategoryType.Journalism
+  },
+  {
+    name: CategoryType.Publishing
+  },
+  {
+    name: CategoryType.Technology
+  },
+  {
+    name: CategoryType.Theater
+  }
+];
--- backend/src/data/seeders/index.ts
+++ backend/src/data/seeders/index.ts
@@ -1,7 +1,11 @@
 import { createConnection } from 'typeorm';
 import { log } from '../../helpers/logger';
 import UserProfileSeeder from './userProfileSeeder';
+import CategorySeeder from './categorySeeder';
 
-createConnection().then(async () => {
-  await UserProfileSeeder.execute();
-}).catch((e) => log(e));
+createConnection()
+  .then(async () => {
+    await UserProfileSeeder.execute();
+    await CategorySeeder.execute();
+  })
+  .catch(e => log(e));
