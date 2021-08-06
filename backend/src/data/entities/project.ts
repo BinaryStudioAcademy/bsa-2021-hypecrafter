41983b53cf0dfa9b728dc06aca47963a0436294c
maksim-luzin <333maximax333@gmail.com>
2021-08-06 17:19:21 +0300

    Edit project entite

--- backend/src/data/entities/project.ts
+++ backend/src/data/entities/project.ts
@@ -43,7 +43,7 @@ export class Project extends AbstractEntity {
   @Column({ type: 'real' })
   minutesToRead: number;
 
-  @Column({ type: 'int' })
+  @Column({ type: 'real' })
   totalInteractionTime: number;
 
   @Column({ type: 'text' })
