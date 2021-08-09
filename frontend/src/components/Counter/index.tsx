7d66c630f6de8980b82bafd417d6652bb4aeba40
dgorkavyi <dima.net.dd@gmail.com>
2021-08-09 15:26:47 +0300

    fix counter animation| formate function replaced from component file in helpers.ts

--- frontend/src/components/Counter/index.tsx
+++ frontend/src/components/Counter/index.tsx
@@ -1,47 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import formate from './helpers';
 import classes from './styles.module.scss';
-
-function nFormatter(num: number, digits: number) {
-  const lookup = [
-    { value: 1, symbol: '' },
-    { value: 1e3, symbol: 'K' },
-    { value: 1e6, symbol: 'M' },
-    { value: 1e9, symbol: 'G' },
-    { value: 1e12, symbol: 'T' },
-    { value: 1e15, symbol: 'P' },
-    { value: 1e18, symbol: 'E' }
-  ];
-  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-  const item = lookup
-    .slice()
-    .reverse()
-    .find((i) => num >= i.value);
-  return item
-    ? (num / item.value).toFixed(digits).replace(rx, '$1') + item.symbol
-    : '0';
-}
 
 const Counter = ({ value }: { value: number }) => {
   const [currVal, setCurrVal] = useState(0);
   const [isChanging, setIsChanging] = useState(false);
 
-  useEffect(() => {
-    setIsChanging(true);
+  const changeInTimeout = (start: number, nextVal: number, time: number) => {
+    const timer = setTimeout(() => {
+      setCurrVal(start + nextVal);
+      console.log(start + nextVal);
+      clearTimeout(timer);
+    }, time);
+  };
 
-    if (currVal < value - 11) {
-      setTimeout(() => setCurrVal(currVal + 11), 1);
+  useEffect(() => {
+    if (currVal < value - 100) {
+      changeInTimeout(currVal, 7, 10);
     } else if (currVal < value) {
-      setTimeout(() => setCurrVal(currVal + 1), 1);
+      changeInTimeout(currVal, 3, 15);
     }
-    if (currVal === value) {
-      setIsChanging(false);
-    }
+
+    setIsChanging(true);
   }, [currVal]);
 
   return (
     <div className={classes.counter}>
       <div>Rating</div>
-      <div className={isChanging ? classes.changing : classes.value}>{nFormatter(currVal, 1)}</div>
+      <div className={isChanging ? classes.changing : classes.value}>
+        {formate(currVal, 2)}
+      </div>
     </div>
   );
 };
