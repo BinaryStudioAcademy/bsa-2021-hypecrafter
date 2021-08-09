6de6dbbd3a4983136c7c1aa685c82eb5073459fd
maksim-luzin <333maximax333@gmail.com>
2021-08-09 12:30:31 +0300

    Edit comments.

--- frontend/src/hooks/useAction.ts
+++ frontend/src/hooks/useAction.ts
@@ -5,22 +5,17 @@ import { authFetchUserAction } from '../actions';
 
 const actions = [authFetchUserAction];
 
-const useActions = () => {
+export const useActions = () => {
   const dispatch = useDispatch();
 
-  return useMemo(() => {
-    if (Array.isArray(actions)) {
-      return actions.map(action => ({
-        [action.name]: {
-          trigger: bindActionCreators(action, dispatch),
-          success: bindActionCreators(action, dispatch),
-          failure: bindActionCreators(action, dispatch)
-        }
-      }));
-    }
-
-    return bindActionCreators(actions, dispatch);
-  }, actions);
+  return useMemo(
+    () => actions.map(action => ({
+      [action.name]: {
+        trigger: bindActionCreators(action, dispatch),
+        success: bindActionCreators(action, dispatch),
+        failure: bindActionCreators(action, dispatch)
+      }
+    })),
+    actions
+  );
 };
-
-export default useActions;
