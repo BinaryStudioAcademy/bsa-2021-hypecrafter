550870251e6833313abae3a9dd7c6cc3542d8c28
maksim-luzin <333maximax333@gmail.com>
2021-08-09 11:46:00 +0300

    Update useAction hook

--- frontend/src/hooks/useAction.ts
+++ frontend/src/hooks/useAction.ts
@@ -1,24 +1,27 @@
-import { bindActionCreators } from 'redux'
+import { bindActionCreators } from 'redux';
 import { useDispatch } from 'react-redux';
 import { useMemo } from 'react';
-import {authFetchUserAction} from '../actions';
+// import {Routine } from '@types/redux-saga-routines'
+import { authFetchUserAction } from '../actions';
 
-const actions = [
-  authFetchUserAction.trigger,
-  authFetchUserAction.success,
-  authFetchUserAction.failure,
-]
+const actions = [authFetchUserAction];
 
 const useActions = () => {
   const dispatch = useDispatch();
 
   return useMemo(() => {
     if (Array.isArray(actions)) {
-      return actions.map(action => bindActionCreators(action, dispatch))
+      return actions.map(action => ({
+        [action.name]: {
+          trigger: bindActionCreators(action, dispatch),
+          success: bindActionCreators(action, dispatch),
+          failure: bindActionCreators(action, dispatch)
+        }
+      }));
     }
 
-    return bindActionCreators(actions, dispatch)
-  }, actions)
-}
+    return bindActionCreators(actions, dispatch);
+  }, actions);
+};
 
 export default useActions;
