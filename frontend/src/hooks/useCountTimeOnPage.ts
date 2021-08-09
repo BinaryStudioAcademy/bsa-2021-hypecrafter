90727daca264049450e51a5c824e97c35a5c8918
dgorkavyi <dima.net.dd@gmail.com>
2021-08-09 23:59:27 +0300

    add array of all timers to find first which be the only one

--- frontend/src/hooks/useCountTimeOnPage.ts
+++ frontend/src/hooks/useCountTimeOnPage.ts
@@ -1,12 +1,15 @@
 import { useEffect, useRef } from 'react';
 
+const timers: NodeJS.Timeout[] = [];
 const useCountTimeOnPage = (callback: (time: number) => void) => {
   const timeRef = useRef(0);
   const timer = setInterval(() => {
     timeRef.current += 1;
   }, 1000);
 
-  if (+`${timer}` !== 1) clearInterval(timer);
+  timers.push(timer);
+
+  if (+`${timer}` !== +`${timers[0]}`) clearInterval(timer);
 
   useEffect(() => () => {
     callback(timeRef.current);
