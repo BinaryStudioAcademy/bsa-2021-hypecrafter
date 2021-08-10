46d74fe75fa88c278a1fe7b3a3a2c7cc9761aa0a
dgorkavyi <dima.net.dd@gmail.com>
2021-08-10 12:46:40 +0300

    add CustomError class

--- gateway/src/api/middlewares/error-handler.ts
+++ gateway/src/api/middlewares/error-handler.ts
@@ -1,9 +1,24 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpStatusCode } from 'hypecrafter-shared/enums/http-status-code';
 import { log } from '../../helpers';
+class CustomError extends Error {
+  constructor(type: string, message: string) {
+    super(message);
+    this.name = type;
+  }
+}
 
-export const handleError = (error: Error, _req: Request, res: Response, _next: NextFunction) => {
+export const handleError = (
+  error: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  const { result } = _req.body;
+
   log(error);
+
+  if (!result) throw new CustomError(error.name, error.message);
 
   res.sendStatus(HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
 };
