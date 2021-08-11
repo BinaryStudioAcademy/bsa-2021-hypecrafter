3ad3e15c4e6d6646f3be462b1f71dd5df4bf67ef
eluyemaStudy <elluyema@gmail.com>
2021-08-11 20:15:37 +0300

    Setting migration

---
+++ payment/src/data/seed-data/transactionData.ts
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+export const transaction = [
+    {
+        name: "Avatar Legends: The Roleplaying Game",
+        type: "Pledge project",
+        total: 343.32,
+        change: -343.32,
+        balance: 123.03
+      },
+      {
+        name: "Hand-Drawn Game Guides",
+        type: "Pledge project",
+        total: 10.1,
+        change: -10.1,
+        balance: 466.35
+      },
+      {
+        name: "Custom Fund",
+        type: "Balance replenishment",
+        total: 400,
+        change: 400,
+        balance: 466.35
+      }
+];
