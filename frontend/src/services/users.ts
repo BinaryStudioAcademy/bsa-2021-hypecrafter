05ab12fff9f9801744484792b16c10c1000bc17f
Nikita <nik2142ua@gmail.com>
2021-08-11 13:22:29 +0300

    Delete comment

--- frontend/src/services/users.ts
+++ frontend/src/services/users.ts
@@ -2,6 +2,4 @@ import { api } from '../helpers';
 
 export const getUsers = () => fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json());
 
-// export const getUser = (id: string) => fetch(`http://localhost:3002/users/${id}`).then(response => response.json());
-
 export const getUser = (id: string) => api.get({ url: `users/${id}` });
