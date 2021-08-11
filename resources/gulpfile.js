7dda28f3ee093382463360fe1cb10732908cde2c
Nikita <nik2142ua@gmail.com>
2021-08-11 13:29:12 +0300

    Change double quotes to single quotes

--- resources/gulpfile.js
+++ resources/gulpfile.js
@@ -1,16 +1,16 @@
-const { task, src, dest } = require("gulp");
-const del = require("del");
-const gulpIf = require("gulp-if");
-const combine = require("gulp-jsoncombine");
-const getData = require("./src/helpers/getData");
+const { task, src, dest } = require('gulp');
+const del = require('del');
+const gulpIf = require('gulp-if');
+const combine = require('gulp-jsoncombine');
+const getData = require('./src/helpers/getData');
 
-const srcPath = "src/resources/**/*.json";
-const outPath = "../backend/src/data/seed-data";
-const outFile = "userProjectData.json";
+const srcPath = 'src/resources/**/*.json';
+const outPath = '../backend/src/data/seed-data';
+const outFile = 'userProjectData.json';
 
-task("default", async () => {
+task('default', async () => {
   await del(`${outPath}/${outFile}`, { force: true });
   return src(srcPath)
-    .pipe(gulpIf("*.json", combine(outFile, getData)))
+    .pipe(gulpIf('*.json', combine(outFile, getData)))
     .pipe(dest(outPath));
 });
