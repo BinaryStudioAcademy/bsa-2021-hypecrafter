85869beb0cc655747bca30a63c35a9fa1b10297a
maksim-luzin <333maximax333@gmail.com>
2021-08-12 11:51:00 +0300

    Add example.

--- frontend/src/components/LeavePage/index.tsx
+++ frontend/src/components/LeavePage/index.tsx
@@ -3,15 +3,25 @@ import { Prompt } from 'react-router-dom';
 import ModalWindow from '../ModalWindow';
 
 interface IUseLeavePage {
-  isBlocking: boolean,
-  message: string
+  isBlocking?: boolean,
+  message?: string
 }
 
 const LeavePage: FC<IUseLeavePage> = ({
-  isBlocking = false,
-  message
+  isBlocking = true,
+  message = 'You must completely create a project.'
 }) => {
   const [isShowModal, setShowModal] = useState(false);
+
+  /*
+    * This is an example of use.
+    *<LeavePage
+    *  message="Your message when blocking. Default 'You must completely create a project.'"
+    *  isBlocking={true/false } Default true.
+    *>
+    *  <CreateProject />
+    *</LeavePage >
+  */
 
   const handleBlockedNavigation = () => {
     if (isBlocking) {
