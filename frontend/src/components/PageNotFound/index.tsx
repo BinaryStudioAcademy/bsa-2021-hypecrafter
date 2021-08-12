1172f97ffeea71728741916863ad131cbec527f1
Demetreus <dima.net.dd@gmail.com>
2021-08-12 13:15:32 +0300

    add localization

--- frontend/src/components/PageNotFound/index.tsx
+++ frontend/src/components/PageNotFound/index.tsx
@@ -1,10 +1,18 @@
+import { Languages } from '../../common/enums';
+import { useLocalization } from '../../providers/localization';
 import classes from './styles.module.scss';
+import Description404 from './translations';
 
-const PageNotFound = () => (
-  <div className={classes.wrapper}>
-    <div className={classes.error}>404</div>
-    <div className={classes.description}>PAGE NOT FOUND</div>
-  </div>
-);
+const PageNotFound = () => {
+  const { selectedLanguage } = useLocalization();
+  const description = Description404[selectedLanguage as Languages];
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.error}>404</div>
+      <div className={classes.description}>{description}</div>
+    </div>
+  );
+};
 
 export default PageNotFound;
