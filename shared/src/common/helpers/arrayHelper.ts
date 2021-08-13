ad883877008f8aa70af25210f210e373ed5b9d07
Yuliia Tkachenko <yulia032022@gmail.com>
2021-08-13 10:54:17 +0300

    remove null from Promise.resolve()

--- shared/src/common/helpers/arrayHelper.ts
+++ shared/src/common/helpers/arrayHelper.ts
@@ -1,4 +1,4 @@
-export const asyncForEach = async <T>(fn: (x: T) => Promise<null>, list: readonly T[]) => {
+export const asyncForEach = async <T>(fn: (x: T) => Promise<void>, list: readonly T[]) => {
   list.reduce(async (listPromise, item) => {
     await listPromise;
     return fn(item);
