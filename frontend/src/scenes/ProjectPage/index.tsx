a0e37432d506b865752e3574964614228aecd83c
Nikita <nik2142ua@gmail.com>
2021-08-13 16:17:21 +0300

    Add Project Page scene base

---
+++ frontend/src/scenes/ProjectPage/index.tsx
@@ -0,0 +1,15 @@
+import { Container, Row } from 'react-bootstrap';
+import Header from './components/Header';
+// import classes from './styles.module.scss';
+
+function ProjectPage() {
+  return (
+    <Container>
+      <Row>
+        <Header />
+      </Row>
+    </Container>
+  );
+}
+
+export default ProjectPage;
