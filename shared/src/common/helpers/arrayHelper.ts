ec53e687f9b9179a9726fefbc08fd975b058585c
Liza <liza140620002@gmail.com>
2021-08-13 15:07:59 +0300

    add and fix seeders

--- shared/src/common/helpers/arrayHelper.ts
+++ shared/src/common/helpers/arrayHelper.ts
@@ -1,12 +1,6 @@
-// export const asyncForEach = async <T>(fn: (x: T) => Promise<void>, list: readonly T[]) => {
-//   list.reduce(async (listPromise, item) => {
-//     await listPromise;
-//     return fn(item);
-//   }, Promise.resolve());
-// };
-/* eslint-disable */
-export const asyncForEach = async <T>(fn: (x: T) => Promise<void>, list: readonly T[]): Promise<void> => {
-  for (let i = 0; i < list.length; i++) {
-    await fn(list[i]);
-  }
+export const asyncForEach = async <T>(fn: (x: T) => Promise<void>, list: readonly T[]) => {
+  list.reduce(async (listPromise, item) => {
+    await listPromise;
+    return fn(item);
+  }, Promise.resolve());
 };
