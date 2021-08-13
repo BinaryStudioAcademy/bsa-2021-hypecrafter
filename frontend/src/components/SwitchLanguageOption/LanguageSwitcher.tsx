7556538a9e24cb9a8899ee3ee1c06a3d48f7544d
dgorkavyi <dima.net.dd@gmail.com>
2021-08-13 15:53:35 +0300

    refactor switchHandler

--- frontend/src/components/SwitchLanguageOption/LanguageSwitcher.tsx
+++ frontend/src/components/SwitchLanguageOption/LanguageSwitcher.tsx
@@ -6,10 +6,7 @@ const LanguageSwitcher = () => {
   const { t, changeLanguage, selectedLanguage: currLang } = useLocalization();
   const otherLanguage = currLang === Languages.EN ? Languages.UA : Languages.EN;
   const currentText = t('Switch to', { lang: otherLanguage.toUpperCase() });
-  const switchHandler = () => {
-    if (currLang === Languages.EN) changeLanguage(Languages.UA);
-    else changeLanguage(Languages.EN);
-  };
+  const switchHandler = () => changeLanguage(otherLanguage);
 
   return (
     <button type="button" className={classes.switchBtn} onClick={switchHandler}>
