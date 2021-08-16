c7a96305b6e79114ac41328458dc8128c897b346
Liza <liza140620002@gmail.com>
2021-08-16 14:44:28 +0300

    add ids to seed data

--- backend/src/data/seed-data/donatorsPrivilegeData.ts
+++ backend/src/data/seed-data/donatorsPrivilegeData.ts
@@ -1,22 +1,18 @@
+/* eslint-disable max-len */
 export const donatorsPrivileges = [
   {
     id: '3688d4de-8f4f-467f-9478-d065cb29e81c',
-    privilege: `1 copy of the product, 
-  plus the Kickstarter exclusive content and all unlocked 
-  standard edition stretch goals.`,
+    privilege: '1 copy of the product, plus the Kickstarter exclusive content and all unlocked standard edition stretch goals.',
     amount: 20
   },
   {
     id: 'bf2cc979-4cbc-40d1-b1df-bc0354818d39',
-    privilege: `Includes ALL unlocked stretch goals! 
-  Shipping and taxes charged after the campaign. 
-  Language selection available in the pledge manager.`,
+    privilege: 'Includes ALL unlocked stretch goals! Shipping and taxes charged after the campaign. Language selection available in the pledge manager.',
     amount: 40
   },
   {
     id: '3ff1465e-04ee-40a2-98c8-9519c79a30b7',
-    privilege: `The box is full of jumbo shop cards, 
-  cardboard items and coins. `,
+    privilege: 'The box is full of jumbo shop cards, cardboard items and coins.',
     amount: 60
   },
   {
