e0fae6e9f29a6513d16e6b1ca8171bbc52bc71e7
eluyemaStudy <elluyema@gmail.com>
2021-08-16 20:38:44 +0300

    fix

--- gateway/src/helpers/uuid.ts
+++ gateway/src/helpers/uuid.ts
@@ -3,6 +3,6 @@ const uuidV4Regex = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-
 export const validateUuid = (route: string, path: string) => {
   const startPointId = route.indexOf('/:id') + 1;
   const endPointId = (path.substr(startPointId)).indexOf('/') + startPointId;
-  const uuid = path.slice(startPointId, endPointId);
+  const uuid = path.slice(startPointId, endPointId === startPointId - 1 ? path.length : endPointId);
   return uuidV4Regex.test(uuid);
 };
