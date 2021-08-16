149d3890e237ff13be85f30752ad4773294786f0
dgorkavyi <dima.net.dd@gmail.com>
2021-08-16 15:23:12 +0300

    add styles

--- frontend/src/components/DatePicker/index.tsx
+++ frontend/src/components/DatePicker/index.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import style from './styles.module.scss';
 
 const DatePickerInput = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const triangle = document.getElementsByClassName('react-datepicker__triangle')[0] as HTMLElement;
+  triangle.style.left = '-7';
   return (
     <DatePicker
+      className={style.input}
       selected={startDate}
       onChange={(date: Date) => setStartDate(date)}
     />
