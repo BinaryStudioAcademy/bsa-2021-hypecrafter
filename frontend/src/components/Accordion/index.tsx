f96fdbe722c0ee37dcfb2171ef6199aebfbaecc9
Liza <liza140620002@gmail.com>
2021-08-17 11:20:07 +0300

    fix accordion's props

--- frontend/src/components/Accordion/index.tsx
+++ frontend/src/components/Accordion/index.tsx
@@ -3,18 +3,23 @@ import { Accordion as AccordionBS } from 'react-bootstrap';
 import classes from './styles.module.scss';
 
 interface Props {
-  question: ReactNode | string;
-  answer: ReactNode | string;
+  accordions:{
+    id: string;
+    question: ReactNode | string;
+    answer: ReactNode | string;
+  }[];
 }
 
-const Accordion: FC<Props> = ({ question, answer }) => (
+const Accordion: FC<Props> = ({ accordions }) => (
   <AccordionBS defaultActiveKey="0" className={classes.main}>
-    <AccordionBS.Item eventKey="1" className={classes.item}>
-      <AccordionBS.Header className={classes.header}>{question}</AccordionBS.Header>
-      <AccordionBS.Body className={classes.body}>
-        {answer}
-      </AccordionBS.Body>
-    </AccordionBS.Item>
+    {accordions.map(({ id, question, answer }) => (
+      <AccordionBS.Item eventKey="1" className={classes.item} key={id}>
+        <AccordionBS.Header className={classes.header}>{question}</AccordionBS.Header>
+        <AccordionBS.Body className={classes.body}>
+          {answer}
+        </AccordionBS.Body>
+      </AccordionBS.Item>
+    ))}
   </AccordionBS>
 );
 
