eeea76ff9a178d1673254dccb3606bf97cd9791f
dgorkavyi <dima.net.dd@gmail.com>
2021-08-20 16:55:20 +0300

    add a few routes

--- gateway/src/common/constants/blackRoutes.ts
+++ gateway/src/common/constants/blackRoutes.ts
@@ -1,11 +1,31 @@
 const BLACK_ROUTES = [
+  '/logout',
   '/add-funds',
-  '/reset-password',
   '/transactions',
+  '/auth/token',
+  '/auth/token/reject',
+  '/payment',
+  '/payment/:id/:page',
+  '/reset-password',
   '/projects/create',
-  '/profile',
-  '/examples',
-  '/payment'
+  '/profile'
 ];
 
 export { BLACK_ROUTES };
+
+// const WHITE_ROUTES = [
+//   '/auth/login',
+//   '/white-rout',
+//   '/auth/token',
+//   '/auth/register',
+//   '/users',
+//   '/users/:id',
+//   '/topics',
+//   '/projects',
+//   '/projects/popular-and-recommended',
+//   '/tags',
+//   '/categories',
+//   '/projects/popular',
+//   '/projects/:id',
+//   '/api-docs'
+// ];
