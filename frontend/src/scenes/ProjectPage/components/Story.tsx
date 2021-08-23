ad98ec22ee6c2ed66fe2fb0754bb450ec6fdb1a5
DarkFlamingo <mgavatuk@gmail.com>
2021-08-23 16:27:04 +0300

    fix style

--- frontend/src/scenes/ProjectPage/components/Story.tsx
+++ frontend/src/scenes/ProjectPage/components/Story.tsx
@@ -1,3 +1,4 @@
+import classnames from 'classnames';
 import { FC } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import ProjectPrivilege from '../../../components/ProjectPrivilege';
@@ -10,65 +11,53 @@ interface StoryProps {
   tags: string[];
 }
 
-const Story: FC<StoryProps> = ({ story, privileges, tags }) => {
-  console.log(story, privileges, tags);
-
-  const data = [
-    'fsadfas',
-    'fsadfdas',
-    'fsaddfas',
-    'fsadfsad',
-    'fsadfasf',
-    'fsdafasd',
-    'fsdafsa',
-    'fdsafasdfas',
-    'fdsafsad',
-    'fsdafsad'
-  ];
-
-  return (
-    <Row>
-      <Col xs={12} lg={8}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industrys standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.s
-      </Col>
-      <Col xs={12} lg={4}>
-        <div className={classes['tag-wrapper']}>
-          {data?.map((el) => (
-            <div key={el} className={classes['tag-item']}>
-              <Tag key={el} text={el} />
-            </div>
-          ))}
-        </div>
-        <Row className={classes['privilege-wrapper']}>
-          {privileges?.map((el) => (
-            <Col
-              xs={12}
-              md={6}
-              lg={12}
-              key={el.amount}
-              className="justify-content-center"
-            >
-              <ProjectPrivilege
-                value={3}
-                title={el.privilege}
-                content="some text"
-                includes={['test1', 'test2']}
-                backers={el.amount}
-              />
-            </Col>
-          ))}
-        </Row>
-      </Col>
-    </Row>
-  );
-};
+const Story: FC<StoryProps> = ({ story, privileges, tags }) => (
+  <Row className={classes['project-story']}>
+    <Col xs={12} lg={8} xl={9}>
+      {story}
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industrys standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.s
+    </Col>
+    <Col xs={12} lg={4} xl={3}>
+      <div className={classes['tag-wrapper']}>
+        {tags?.map((el) => (
+          <div key={el} className={classes['tag-item']}>
+            <Tag key={el} text={el} />
+          </div>
+        ))}
+      </div>
+      <p className={classes['privilege-title']}>Privileges</p>
+      <Row className={classes['privilege-wrapper']}>
+        {privileges?.map((el) => (
+          <Col
+            xs={12}
+            md={6}
+            lg={12}
+            key={el.amount}
+            className={classnames(
+              classes['privilege-item'],
+              'justify-content-center'
+            )}
+          >
+            <ProjectPrivilege
+              value={3}
+              title={el.privilege}
+              content="some text"
+              includes={['test1', 'test2']}
+              backers={el.amount}
+            />
+          </Col>
+        ))}
+      </Row>
+    </Col>
+  </Row>
+);
 
 export default Story;
