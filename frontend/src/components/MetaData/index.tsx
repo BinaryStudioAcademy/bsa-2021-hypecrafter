dc96ce190b5f81be614701d83b7abe335cd683e9
maksim-luzin <333maximax333@gmail.com>
2021-08-25 18:07:29 +0300

    Add localization.

--- frontend/src/components/MetaData/index.tsx
+++ frontend/src/components/MetaData/index.tsx
@@ -7,17 +7,24 @@ const MetaData = () => {
   const siteName = 'HypeCrafter';
   const title = t('Social title');
   const siteDescription = t('Agreement');
-  const logoLink = 'http://hypecrafter2-env.eba-n3gbu5mb.us-west-2.elasticbeanstalk.com/HypeCoin.png';
   const link = 'http://hypecrafter2-env.eba-n3gbu5mb.us-west-2.elasticbeanstalk.com';
+  const logoLink = () => {
+    switch (t('language')) {
+      case 'uk':
+        return 'http://s1.litepic.ru/121082524d7bdd3f6f2bc8898345.png';
+      default:
+        return 'http://s1.litepic.ru/121082524293df507f813f618cae.png';
+    }
+  };
 
   return (
     <Helmet>
-      <meta property='og:locale' content='en_US' />
+      <meta property='og:locale' content={t('Site locale')} />
       <meta property='og:type' content='website' />
       <meta property='og:site_name' content={siteName} />
       <meta property='og:title' content={title} />
       <meta property='og:description' content={siteDescription} />
-      <meta property='og:image' content={logoLink} />
+      <meta property='og:image' content={logoLink()} />
       <meta property='og:url' content={link} />
     </Helmet>
   );
