24287abe0f0e3a6dbc1044ece13cbecb7e1f6aba
Cuba49 <kubovych.kostya59@gmail.com>
2021-08-25 16:54:48 +0300

    add upload on aws

--- frontend/src/components/ImageUpload/index.tsx
+++ frontend/src/components/ImageUpload/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import S3FileUpload from 'react-s3';
 import Button from '../Button';
 
 interface Prors{
@@ -6,13 +7,18 @@ interface Prors{
   id: string,
   label:string
 }
-
+const config = {
+  bucketName: 'hypecrafter',
+  region: 'eu-west-2',
+  accessKeyId: 'AKIA2MWJ7PBCMN3574WG',
+  secretAccessKey: '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'
+};
 const ImageUpload:FC<Prors> = ({ onFileChange, id, label }) => {
   const imageHandler = (files: any) => {
     const file = files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => { if (reader.result) onFileChange(reader.result.toString()); };
+    S3FileUpload.uploadFile(file, config)
+      .then((data:any) => { onFileChange(data.location); })
+      .catch((err:any) => console.log(err));
   };
   return (
     <Button>
