4eb0fb664f3515f5f2c6228d0dcc872281c4b72e
Liza <liza140620002@gmail.com>
2021-08-27 09:36:17 +0300

    refactor project service

--- frontend/src/services/project.ts
+++ frontend/src/services/project.ts
@@ -2,7 +2,9 @@ import { Project } from '../common/types';
 import { api } from '../helpers/http';
 
 interface GetProjectProps { id: string; userId: string; }
+
 interface SetReactionProps { isLiked: boolean | null; projectId: string; }
+
 interface SetWatchProps { isWatched: boolean | null; projectId: string; }
 
 export const createProject = async (body: any) => {
@@ -11,16 +13,10 @@ export const createProject = async (body: any) => {
   return project;
 };
 
-export const getProject = async (params: GetProjectProps) => {
-  const project: Project = await api.get({ url: `projects/${params.id}`, params });
+export const getProject = (
+  params: GetProjectProps
+): Promise<Project> => api.get({ url: `projects/${params.id}`, params });
 
-  return project;
-};
+export const setReaction = (params : SetReactionProps) => api.post({ url: 'projects/reaction', params });
 
-export const setReaction = (
-  params : SetReactionProps
-) => api.post({ url: 'projects/reaction', params });
-
-export const setWatch = async (params: SetWatchProps) => {
-  await api.post({ url: 'projects/watch', params });
-};
+export const setWatch = (params: SetWatchProps) => api.post({ url: 'projects/watch', params });
