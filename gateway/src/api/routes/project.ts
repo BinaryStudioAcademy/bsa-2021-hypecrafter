e255326fb506577ae775817acda7e7535545d480
maksim-luzin <333maximax333@gmail.com>
2021-09-02 03:04:13 +0300

    Add gateway rotes

--- gateway/src/api/routes/project.ts
+++ gateway/src/api/routes/project.ts
@@ -5,7 +5,8 @@ const init = () => {
   const router = Router();
   return router
     .get(['/', '/:id'], (_, res: Response) => res.delegate(Project.BACKEND))
-    .post(['/', '/reaction', '/watch'], (_, res: Response) => res.delegate(Project.BACKEND));
+    .post(['/', '/reaction', '/watch'], (_, res: Response) => res.delegate(Project.BACKEND))
+    .put(['/', '/views-interaction', '/:id'], (_, res: Response) => res.delegate(Project.BACKEND));
 };
 
 export default init;
