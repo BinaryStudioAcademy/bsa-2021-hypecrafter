2f5ef951a729bf26b44d48c56318836344401485
maksim-luzin <333maximax333@gmail.com>
2021-09-03 04:02:32 +0300

    Edit gateway

--- gateway/src/api/routes/project.ts
+++ gateway/src/api/routes/project.ts
@@ -6,7 +6,7 @@ const init = () => {
   return router
     .get(['/', '/:id'], (_, res: Response) => res.delegate(Project.BACKEND))
     .post(['/', '/reaction', '/watch'], (_, res: Response) => res.delegate(Project.BACKEND))
-    .put(['/', '/views-interaction', '/:id'], (_, res: Response) => res.delegate(Project.BACKEND));
+    .put('/views-interaction/:id', (_, res: Response) => res.delegate(Project.BACKEND));
 };
 
 export default init;
