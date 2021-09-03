4717757421176657a3b6da7dd515c88644013847
Nikita <nik2142ua@gmail.com>
2021-09-03 21:34:48 +0300

    Add Tab navigation between Popular and Recommended

--- mobile/HypecrafterMobile/app/components/MainView/MainView.tsx
+++ mobile/HypecrafterMobile/app/components/MainView/MainView.tsx
@@ -1,7 +1,9 @@
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useTypedSelector } from '../../hooks/store';
 import { useAction } from '../../hooks/useAction';
+import PopularScreen from './screens/PopularScreen';
+import RecommendedScreen from './screens/RecommendedScreen';
 
 const MainView = ({navigation} : {navigation: any}) => {
   const { fetchPopularAndRecommendedProjectsAction, fetchTopics } = useAction();
@@ -18,9 +20,7 @@ const MainView = ({navigation} : {navigation: any}) => {
     topics: mainPage.topics,
   }));
 
-  const onLogOut = async () => {
-    navigation.navigate('Auth');
-  };
+  const Tab = createMaterialTopTabNavigator();
 
   useEffect(() => {
     fetchPopularAndRecommendedProjectsAction();
@@ -28,34 +28,22 @@ const MainView = ({navigation} : {navigation: any}) => {
   }, []);
   
   return (
-    <View style={styles.wrapper}>
-       <TouchableOpacity style={styles.button} onPress={onLogOut}>
-        <Text style={styles.buttonText}>Back to login</Text>
-      </TouchableOpacity>
-      <Text>Main page</Text>
-    </View>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { backgroundColor: '#474a4f' },
+        tabBarLabelStyle: { fontWeight: '500', color: 'white' },
+      }}
+    >
+      <Tab.Screen
+        name="Popular"
+        children={() => (<PopularScreen popularStartups={popularStartups}/>)}
+      />
+      <Tab.Screen
+        name="Recommended"
+        children={() => (<RecommendedScreen recommendedStartups={recommendedStartups}/>)}
+      />
+    </Tab.Navigator>
   );
 };
-
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: 'blue',
-    borderColor: 'white',
-    alignItems: 'center',
-    padding: 10,
-    borderBottomWidth: 0,
-    borderRadius: 5,
-    width: '30%',
-    marginBottom: 15,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: '700',
-  },
-});
 
 export default MainView;
