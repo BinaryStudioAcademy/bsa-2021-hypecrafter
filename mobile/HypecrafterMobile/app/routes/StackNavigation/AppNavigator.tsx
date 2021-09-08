388e04043d9285684600a9538b7cc4769ecc7577
Nikita <nik2142ua@gmail.com>
2021-09-08 11:48:14 +0300

    Update AppNavigator component

--- mobile/HypecrafterMobile/app/routes/StackNavigation/AppNavigator.tsx
+++ mobile/HypecrafterMobile/app/routes/StackNavigation/AppNavigator.tsx
@@ -1,14 +1,20 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import MainView from '../../components/MainView/MainView';
+import Header from '../../components/common/Header'
+import commonStyles from '../../styles/common';
 
 const AppNavigator = createStackNavigator(
   {
     Main: {
       screen: MainView,
       navigationOptions: {
-        headerShown: false,
+        headerStyle: {
+          backgroundColor: commonStyles.color.grey,
+        },
+        headerTitle: () => <Header />
       },
-    },
+    }
   },
   {
     initialRouteName: 'Main',
