384b1b426d52bebbc130cf6f5f5094ba849c78fe
maksim-luzin <333maximax333@gmail.com>
2021-09-09 19:20:25 +0300

    Update backend part hide empty tag

--- backend/src/services/project/project.ts
+++ backend/src/services/project/project.ts
@@ -1,9 +1,16 @@
-import { ProjectsCategories, ProjectsFilter, ProjectsSort } from 'hypecrafter-shared/enums';
+import { ProjectsFilter, ProjectsSort } from 'hypecrafter-shared/enums';
 import { Project } from '../../common/types';
 import { Chat, Project as CreateProject, Team } from '../../data/entities';
-import { mapPrivileges, mapProjects } from '../../data/mappers';
+import { mapPrivileges, mapProjects, mapBoolean, nullToNumber } from '../../data/mappers';
 import { mapLikesAndDislikes } from '../../data/mappers/mapLikesAndDislikes';
-import { ChatRepository, ProjectRepository, TeamRepository, UserRepository } from '../../data/repositories';
+import {
+  ChatRepository, ProjectRepository,
+  TeamRepository, UserRepository
+} from '../../data/repositories';
+import FAQServise from '../faq';
+import DonatorsPrivilegeServise from '../projectPrivilege';
+import ProjectTagService from '../projectTag';
+import TagService from '../tag';
 
 export default class ProjectService {
   readonly #projectRepository: ProjectRepository;
@@ -14,16 +21,26 @@ export default class ProjectService {
 
   readonly #userRepository: UserRepository;
 
-  constructor(
-    projectRepository: ProjectRepository,
-    teamRepository: TeamRepository,
-    chatRepository: ChatRepository,
-    userRepository: UserRepository
-  ) {
+  readonly #tagService: TagService;
+
+  readonly #projectTagService: ProjectTagService;
+
+  readonly #donatorsPrivilegeServise: DonatorsPrivilegeServise;
+
+  readonly #faqServise: FAQServise;
+
+  constructor(projectRepository: ProjectRepository, teamRepository: TeamRepository,
+    chatRepository: ChatRepository, userRepository: UserRepository,
+    tagService: TagService, projectTagService: ProjectTagService,
+    donatorsPrivilegeServise: DonatorsPrivilegeServise, faqServise: FAQServise) {
     this.#projectRepository = projectRepository;
     this.#teamRepository = teamRepository;
     this.#chatRepository = chatRepository;
     this.#userRepository = userRepository;
+    this.#tagService = tagService;
+    this.#projectTagService = projectTagService;
+    this.#donatorsPrivilegeServise = donatorsPrivilegeServise;
+    this.#faqServise = faqServise;
   }
 
   public async getPopularProjectsByCategory(category: string) {
@@ -42,37 +59,51 @@ export default class ProjectService {
   }
 
   public async createProject(body: CreateProject) {
-    const project: CreateProject = await this.#projectRepository.save({ ...body });
+    const project: CreateProject = await this.#projectRepository.save(body);
     const team = await this.#teamRepository.save({ ...new Team(), ...body.team, project });
     this.#chatRepository.save(body.team.chats.map(chat => ({ ...new Chat(), ...chat, team })));
+    const listTags = await this.#tagService.save(body.projectTags.map(projectTag => projectTag.tag));
+    await this.#projectTagService.remove(project.projectTags.map(projectTag => projectTag.id));
+    await this.#projectTagService.save(listTags.map(tag => ({ tag, project })));
+    await this.#donatorsPrivilegeServise.save(body.donatorsPrivileges.map(privilege => ({ ...privilege, project })));
+    await this.#faqServise.save(body.faqs.map(faq => ({ ...faq, project })));
     return project;
   }
 
-  public async getBySortAndFilter({ sort, filter, category, userId }: {
+  public async getBySortAndFilter({ sort, filter, stringifiedCategories, userId }: {
     sort: ProjectsSort,
     filter: ProjectsFilter,
-    category: ProjectsCategories,
+    stringifiedCategories: string,
     userId?: string,
   }) {
-    const projects: Project[] = await this.#projectRepository.getBySortAndFilter({ sort, filter, category, userId });
+    const categories = JSON.parse(stringifiedCategories);
+    const projects: Project[] = await this.#projectRepository.getBySortAndFilter({ sort, filter, categories, userId });
     return projects;
   }
 
   public async getById(id: string, userId: string | undefined) {
     const project = await this.#projectRepository.getById(id, userId);
-    project.bakersAmount = Math.max(0, project.bakersAmount);
-    project.donated = Math.max(0, project.donated);
-    project.privileges = mapPrivileges(project.privileges, project.bakersDonation);
 
-    return project; // rewrite when error handling middleware works
+    return {
+      ...project,
+      tags: mapBoolean(project.tags),
+      donated: nullToNumber(project.donated),
+      bakersAmount: nullToNumber(project.bakersAmount),
+      privileges: mapPrivileges(project.privileges, project.bakersDonation)
+    }; // rewrite when error handling middleware works // rewrite when error handling middleware works
+  }
+
+  public async getForEdit(id: string) {
+    const project: CreateProject = await this.#projectRepository.getForEdit(id);
+    return project;
   }
 
   public async setReaction({ isLiked, projectId }: { isLiked: boolean, projectId: string }, userId: string) {
     const project = await this.#projectRepository.findOne({ id: projectId });
     const user = await this.#userRepository.findOne({ id: userId });
     await this.#projectRepository.setReaction(isLiked, user, project);
-    const likesAndDislikes:
-    { likes: string, dislikes: string } = await this.#projectRepository.getLikesAndDislikesAmount(project.id);
+    const likesAndDislikes: { likes: string, dislikes: string } = await this
+      .#projectRepository.getLikesAndDislikesAmount(project.id);
 
     return mapLikesAndDislikes(likesAndDislikes);
   }
