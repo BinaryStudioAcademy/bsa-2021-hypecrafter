ef8fc0feac2c7cc0b8b8d0fb1211f807f9222abc
Liza <liza140620002@gmail.com>
2021-09-09 15:47:10 +0300

    fix style, add story

--- mobile/HypecrafterMobile/app/components/ProjectView/screens/Story.tsx
+++ mobile/HypecrafterMobile/app/components/ProjectView/screens/Story.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import AutoHeightWebView from 'react-native-autoheight-webview';
 
-const StoryView = () => {
-
+const StoryView = ({ story }: { story: string }) => {
   return (
-    <View>
-      <Text style={{ color: 'white' }}>Story</Text>
+    <View style={{ flex: 1, flexDirection: 'column' }}>
+      <AutoHeightWebView
+        customStyle={`
+          .container {
+            padding-right: 20px;
+            color: white;
+          }
+          p {
+            font-size: 16px;
+          }
+        `}
+        source={{
+          html: `<div class="container">${story}</div>`
+        }}
+      />
     </View>
   )
 }
