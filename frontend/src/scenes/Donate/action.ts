a353e6d79f382c836690c863a892084ae3e252c4
eluyemaStudy <elluyema@gmail.com>
2021-09-09 10:03:06 +0300

    add action hideDonateModal

--- frontend/src/scenes/Donate/action.ts
+++ frontend/src/scenes/Donate/action.ts
@@ -2,6 +2,7 @@ import { createRoutine } from 'redux-saga-routines';
 
 export enum WalletActions {
   SHOW_DONATE_MODAL = 'DONATE/SHOW_DONATE_MODAL',
+  HIDE_DONATE_MODAL = 'DONATE/HIDE_DONATE_MODAL',
   EXECUTE_DONATE = 'DONATE/EXECUTE_DONATE'
 }
 
@@ -9,11 +10,14 @@ export const showDonateModalAction = createRoutine(WalletActions.SHOW_DONATE_MOD
   trigger: (projectId: string) => ({ projectId })
 });
 
+export const hideDonateModalAction = createRoutine(WalletActions.HIDE_DONATE_MODAL);
+
 export const executeDonateAction = createRoutine(WalletActions.EXECUTE_DONATE, {
   trigger: (projectId: string, amount: number) => ({ projectId, amount }),
   success: (success: boolean) => ({ success })
 });
 
+export type HideDonateModalActionTrigger = ReturnType<typeof hideDonateModalAction.trigger>;
 export type ShowDonateModalActionTrigger = ReturnType<typeof showDonateModalAction.trigger>;
 export type ExecuteDonateActionTrigger = ReturnType<typeof executeDonateAction.trigger>;
 export type ExecuteDonateActionSuccess = ReturnType<typeof executeDonateAction.success>;
