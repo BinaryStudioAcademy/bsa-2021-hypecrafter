0b740840128c4f3af48e1718f0afc816313908c8
eluyemaStudy <elluyema@gmail.com>
2021-09-09 10:30:15 +0300

    Change some naming in actions

--- frontend/src/scenes/Donate/actions.ts
+++ frontend/src/scenes/Donate/actions.ts
@@ -1,18 +1,18 @@
 import { createRoutine } from 'redux-saga-routines';
 
-export enum WalletActions {
+export enum DonateActions {
   SHOW_DONATE_MODAL = 'DONATE/SHOW_DONATE_MODAL',
   HIDE_DONATE_MODAL = 'DONATE/HIDE_DONATE_MODAL',
   EXECUTE_DONATE = 'DONATE/EXECUTE_DONATE'
 }
 
-export const showDonateModalAction = createRoutine(WalletActions.SHOW_DONATE_MODAL, {
+export const showDonateModalAction = createRoutine(DonateActions.SHOW_DONATE_MODAL, {
   trigger: (projectId: string) => ({ projectId })
 });
 
-export const hideDonateModalAction = createRoutine(WalletActions.HIDE_DONATE_MODAL);
+export const hideDonateModalAction = createRoutine(DonateActions.HIDE_DONATE_MODAL);
 
-export const executeDonateAction = createRoutine(WalletActions.EXECUTE_DONATE, {
+export const executeDonateAction = createRoutine(DonateActions.EXECUTE_DONATE, {
   trigger: (projectId: string, amount: number) => ({ projectId, amount }),
   success: (success: boolean) => ({ success })
 });
